1b4a9634850909ad0d568e619fb67f757d746fbb
ImpulseModz <107946862+ImpulseModz@users.noreply.github.com>
2023-05-17 23:36:41 -0600

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,7 @@
+client_script 'config.lua'
+client_script 'client.lua'
+server_script "config.lua"
+server_script "server.lua"
+
+server_export "HideUserTag"
+server_export "ShowUserTag"
---
+++ config.lua
@@ -0,0 +1,16 @@
+Config = {
+	Prefix = '^9[^1Badger-Tags^9] ^3',
+	TagsForStaffOnly = false, -- "DiscordTagIDs.Use.Tag-Toggle"
+	ShowOwnTag = true, -- Should the tag also be shown for own user?
+	UseDiscordName = true,
+	ShowDiscordDescrim = false, -- Should it show Badger#0002 ?
+	FormatDisplayName = "{PLAYER_NAME} [{SERVER_ID}]",
+	UseKeyBind = true, -- It will only show on keybind press
+	KeyBind = 10, -- Pageup -- USE https://docs.fivem.net/docs/game-references/controls/ for keycodes
+	roleList = {
+		{0, "~g~Member ~w~"},  
+		{597449475547463681, "~b~Developer ~w~"},
+		{597450498060058624, "~r~STAFF ~w~"},  
+		{"Founder", "~o~FOUNDER ~w~"}, 
+	},
+}
